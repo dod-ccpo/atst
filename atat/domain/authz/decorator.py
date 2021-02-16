fc8eafa96289f1d65a45b0f63e269b4e4e9ede10
Anh Tran <anhtran@Anhs-MBP.fios-router.home>
2021-02-16 12:55:02 -0500

    revert decorator change

--- atat/domain/authz/decorator.py
+++ atat/domain/authz/decorator.py
@@ -18,7 +18,9 @@ def check_access(permission, message, override, *args, **kwargs):
     if override is not None and override(g.current_user, **access_args, **kwargs):
         return True
 
-    return user_can_access(g.current_user, permission, **access_args)
+    user_can_access(g.current_user, permission, **access_args)
+
+    return True
 
 
 def user_can_access_decorator(permission, message=None, override=None):
@@ -34,6 +36,7 @@ def user_can_access_decorator(permission, message=None, override=None):
                     request.path,
                     extra={"tags": ["access", "success"]},
                 )
+
                 return f(*args, **kwargs)
             except UnauthorizedError as err:
                 app.logger.warning(
@@ -43,6 +46,7 @@ def user_can_access_decorator(permission, message=None, override=None):
                     request.path,
                     extra={"tags": ["access", "failure"]},
                 )
+
                 raise err
 
         return decorated_function
