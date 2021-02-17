c1df9fc180cdbe71550051a8f9d27787fa2bdb6d
Anh Tran <anh.tran-ctr@ccpo.mil>
2021-02-17 17:38:21 -0500

    added another assertion

--- tests/routes/test_dev.py
+++ tests/routes/test_dev.py
@@ -1,11 +1,12 @@
-from atat.routes.dev import _DEV_USERS
+from atat.routes.dev import _DEV_USERS as DEV_USERS
 
 
 def test_users():
     """
     Validates users in _DEV_USERS
     """
-    names = [user["first_name"] for user in _DEV_USERS.values()]
-    for user in _DEV_USERS.values():
+    names = [user["first_name"] for user in DEV_USERS.values()]
+    for user in DEV_USERS.values():
         assert user["citizenship"] == "United States"
+        assert user["designation"] == "military"
         assert user["first_name"] in names
