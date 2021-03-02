914e51e4d654c54a6adaa517e1cd224388f0b960
Kyle Laker <kyle.laker-ctr@ccpo.mil>
2021-03-01 22:16:15 -0500

    Enforce policies as clarified in the docs
    https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet

--- terraform/providers/application_env/k8s.tf
+++ terraform/providers/application_env/k8s.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "aks" {
   resource_group_name                            = azurerm_resource_group.vpc.name
   virtual_network_name                           = azurerm_virtual_network.vpc.name
   address_prefixes                               = ["10.1.2.0/24"]
-  enforce_private_link_endpoint_network_policies = false
+  enforce_private_link_endpoint_network_policies = true
   service_endpoints = [
     "Microsoft.Storage",
     "Microsoft.KeyVault",
