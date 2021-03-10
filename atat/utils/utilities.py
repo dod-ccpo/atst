fc4e8bdb65d8e16998c7294e54f21a742c6758f4
Anh Tran <anh.tran-ctr@ccpo.mil>
2021-03-10 15:31:54 -0500

    Corrected documentation on sorting method
    Ticket: AT-5997

--- atat/utils/utilities.py
+++ atat/utils/utilities.py
@@ -2,7 +2,6 @@ def sort_nested(lst):
     """
     This method is for sorting a list of lists. As of now, this will work as long as
     the inner lists are the same depth and all of the typings are the same.
-    The contents must be a list of a list
 
     sort_nested(["a, "b", c"]) -> Valid
     sort_nested([["a", "b", "c"], ["d", "e", "f"]]) -> Valid
